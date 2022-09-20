af3c8cc96f58c38cbf8e2a1709a608ec3e8504e7
ElrohirGT <elrohirgt@gmail.com>
2022-09-20 04:12:14 -0600

    Added an enums parser

--- src/enums.rs
+++ src/enums.rs
@@ -1,5 +1,17 @@
-use crate::field::doesnt_have_spaces;
+use nom::bytes::complete::tag;
+use nom::bytes::complete::take_till;
+use nom::bytes::complete::take_while;
+use nom::character::complete::char;
+use nom::multi::many0;
+use nom::sequence::delimited;
+use nom::sequence::terminated;
 use nom::IResult;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct PlantUMLEnum<'a> {
+    pub name: &'a str,
+    pub variants: Vec<PlantUMLEnumVariant<'a>>,
+}
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct PlantUMLEnumVariant<'a> {
@@ -7,13 +19,73 @@ pub struct PlantUMLEnumVariant<'a> {
 }
 
 pub fn parse_enum_variant(element: &str) -> IResult<&str, PlantUMLEnumVariant> {
-    let (rest, name) = doesnt_have_spaces(("", element.trim()))?;
+    let (rest, name) = terminated(
+        take_till(|c: char| c.is_whitespace() || c == '}'),
+        char('\n'),
+    )(element.trim_start())?;
     Ok((rest, PlantUMLEnumVariant { name }))
+}
+
+pub fn parse_enum(element: &str) -> IResult<&str, PlantUMLEnum> {
+    let (rest, name) = delimited(
+        tag("enum "),
+        take_while(|c: char| !c.is_whitespace()),
+        tag(" {"),
+    )(element)?;
+
+    let (rest, variants) = terminated(many0(parse_enum_variant), tag("}"))(rest)?;
+
+    Ok((rest, PlantUMLEnum { name, variants }))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn parse_enum_succeeds() {
+        let input = "enum AccionUsuario {
+    SALIR
+    TOTAL_GOLES
+    TOTAL_TIROS_ESQUINA
+}\n";
+        let (rest, enu) = parse_enum(input).expect("Couldn't parse the enum!");
+        assert_eq!("\n", rest);
+        assert_eq!(
+            enu,
+            PlantUMLEnum {
+                name: "AccionUsuario",
+                variants: vec![
+                    PlantUMLEnumVariant { name: "SALIR" },
+                    PlantUMLEnumVariant {
+                        name: "TOTAL_GOLES"
+                    },
+                    PlantUMLEnumVariant {
+                        name: "TOTAL_TIROS_ESQUINA"
+                    },
+                ]
+            }
+        )
+    }
+
+    #[test]
+    fn parse_enum_fails() {
+        let input = "enum Accion Usuario {
+    SALIR
+    TOTAL_GOLES
+    TOTAL_TIROS_ESQUINA
+}";
+        let res = parse_enum(input);
+        assert_eq!(true, res.is_err());
+
+        let input = "enum Accion Usuario {
+    SALIR
+    TOTAL GOLES
+    TOTAL_TIROS_ESQUINA
+}";
+        let res = parse_enum(input);
+        assert_eq!(true, res.is_err());
+    }
 
     #[test]
     fn parse_enum_variant_succeeds() {
--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,8 @@ pub mod interfaces;
 pub mod methods;
 pub mod modifiers;
 
-use crate::enums::PlantUMLEnumVariant;
 use crate::field::parse_field;
 use crate::field::PlantUMLField;
 use crate::methods::parse_method;
 use crate::methods::PlantUMLMethod;
 use nom::IResult;
-
-#[derive(Debug)]
-pub enum UMLElement<'a> {
-    UMLField(PlantUMLField<'a>),
-    UMLMethod(PlantUMLMethod<'a>),
-    UMLEnumVariant(PlantUMLEnum<'a>),
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct PlantUMLEnum<'a> {
-    pub variants: Vec<PlantUMLEnumVariant<'a>>,
-}
